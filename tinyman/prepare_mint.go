ec53e701694ad45dfe9dc4476cb53f2f18e5a175
synycboom <synycboom@gmail.com>
2022-04-08 00:53:20 +0700

    feat: implement prepare mint transaction wrapper

---
+++ tinyman/prepare_mint.go
@@ -0,0 +1,78 @@
+package tinyman
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/synycboom/tinyman-go-sdk/v1/prepare"
+)
+
+// MintTransactions prepares a transaction group to mint the liquidity pool asset amount in exchange for pool assets.
+// validatorappID, asset1ID, asset2ID, liquidityAssetID, asset1Amount, asset2Amount, liquidityAssetAmount are converted to uint64
+func MintTransactions(
+	validatorAppID,
+	asset1ID,
+	asset2ID,
+	liquidityAssetID,
+	asset1Amount,
+	asset2Amount,
+	liquidityAssetAmount,
+	senderAddress string,
+	suggestedParams *SuggestedParams,
+) (*TransactionGroup, error) {
+	if suggestedParams == nil {
+		return nil, fmt.Errorf("suggestedParams is required")
+	}
+
+	uintValidatorAppID, err := strconv.ParseUint(validatorAppID, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintAsset1ID, err := strconv.ParseUint(asset1ID, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintAsset2ID, err := strconv.ParseUint(asset2ID, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintLiquidityAssetID, err := strconv.ParseUint(liquidityAssetID, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintAsset1Amount, err := strconv.ParseUint(asset1Amount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintAsset2Amount, err := strconv.ParseUint(asset2Amount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	uintLiquidityAssetAmount, err := strconv.ParseUint(liquidityAssetAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	txGroup, err := prepare.MintTransactions(
+		uintValidatorAppID,
+		uintAsset1ID,
+		uintAsset2ID,
+		uintLiquidityAssetID,
+		uintAsset1Amount,
+		uintAsset2Amount,
+		uintLiquidityAssetAmount,
+		senderAddress,
+		*suggestedParams.wrapped,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TransactionGroup{wrapped: txGroup}, nil
+}
