be3180000baa632f92ab1929b8c34f93648a0dd3
synycboom <synycboom@gmail.com>
2022-04-08 00:47:45 +0700

    feat: add constants

---
+++ tinyman/const.go
@@ -0,0 +1,24 @@
+package tinyman
+
+const (
+	// TinyManURL is a tiny man web url
+	TinyManURL = "https://tinyman.org"
+
+	// AlgodTestnetHost is the algorand test net url
+	AlgodTestnetHost = "https://testnet-api.algonode.cloud"
+
+	// AlgodMainnetHost is the algorand main net url
+	AlgodMainnetHost = "https://mainnet-api.algonode.cloud"
+
+	// TestnetValidatorAppIdV1_1 is the Tinyman test net validator app id version 1.1
+	TestnetValidatorAppIdV1_1 int = 62368684
+
+	// MainnetValidatorAppIdV1_1 is the Tinyman main net validator app id version 1.1
+	MainnetValidatorAppIdV1_1 int = 552635992
+
+	// TestnetValidatorAppId is an alias for the current Tinyman test net validator app id
+	TestnetValidatorAppId = TestnetValidatorAppIdV1_1
+
+	// MainnetValidatorAppId is an alias for the current Tinyman main net validator app id
+	MainnetValidatorAppId = MainnetValidatorAppIdV1_1
+)
