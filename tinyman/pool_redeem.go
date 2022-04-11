0d6c1e255319528d736bab89f7489c4229c4a854
synycboom <synycboom@gmail.com>
2022-04-11 14:58:45 +0700

    feat: implement redeem quotes filter functions

--- tinyman/pool_redeem.go
+++ tinyman/pool_redeem.go
@@ -3,6 +3,9 @@ package tinyman
 import (
 	"context"
 	"fmt"
+	"strconv"
+
+	"github.com/synycboom/tinyman-go-sdk/types"
 )
 
 // PrepareRedeemTransactions prepares redeem transaction and returns a transaction group
@@ -17,4 +20,74 @@ func (p *Pool) PrepareRedeemTransactions(amountOut *AssetAmount, redeemerAddress
 	}
 
 	return &TransactionGroup{wrapped: txGroup}, nil
+}
+
+// PrepareRedeemTransactionsFromQuote prepares redeem transactions and return a transaction group from quote
+func (p *Pool) PrepareRedeemTransactionsFromQuote(quote *RedeemQuote, redeemerAddress string) (*TransactionGroup, error) {
+	if quote == nil {
+		return nil, fmt.Errorf("quote is required")
+	}
+
+	txGroup, err := p.wrapped.PrepareRedeemTransactionsFromQuote(context.Background(), *quote.wrapped, redeemerAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TransactionGroup{wrapped: txGroup}, nil
+}
+
+// FilterRedeemQuotes filters only redeem quote that belonging to this pool
+// It returns a new RedeemQuoteIterator
+func (p *Pool) FilterRedeemQuotes(iter *RedeemQuoteIterator) (*RedeemQuoteIterator, error) {
+	if iter == nil {
+		return nil, fmt.Errorf("iter is required")
+	}
+
+	var quotes []types.RedeemQuote
+	for iter.HasNext() {
+		item := iter.Next()
+		quotes = append(quotes, *item.wrapped)
+	}
+
+	quotes, err := p.wrapped.FilterRedeemQuotes(quotes)
+	if err != nil {
+		return nil, err
+	}
+
+	var wrappedQuotes []*RedeemQuote
+	for _, quote := range quotes {
+		wrappedQuotes = append(wrappedQuotes, &RedeemQuote{wrapped: &quote})
+	}
+
+	return &RedeemQuoteIterator{
+		values: wrappedQuotes,
+	}, nil
+}
+
+// GetRedeemQuoteMatchesAssetID filters redeem quote belonging to this pool which matches an asset id
+// assetID is converted to uint64
+func (p *Pool) GetRedeemQuoteMatchesAssetID(assetID string, iter *RedeemQuoteIterator) (*RedeemQuote, error) {
+	if iter == nil {
+		return nil, fmt.Errorf("iter is required")
+	}
+
+	uintAssetID, err := strconv.ParseUint(assetID, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	var quotes []types.RedeemQuote
+	for iter.HasNext() {
+		item := iter.Next()
+		quotes = append(quotes, *item.wrapped)
+	}
+
+	quote, err := p.wrapped.GetRedeemQuoteMatchesAssetID(uintAssetID, quotes)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RedeemQuote{
+		wrapped: quote,
+	}, nil
 }
--- tinyman/redeem_quote.go
+++ tinyman/redeem_quote.go
@@ -2,6 +2,39 @@ package tinyman
 
 import "github.com/synycboom/tinyman-go-sdk/types"
 
+// RedeemQuoteIterator is an iterator for iterating redeem quotes
+type RedeemQuoteIterator struct {
+	curr   int
+	values []*RedeemQuote
+}
+
+// HasNext return true if there are items to be iterated
+func (i *RedeemQuoteIterator) HasNext() bool {
+	return i.curr < len(i.values)
+}
+
+// Next returns the next a item, returns nil if no item left
+func (i *RedeemQuoteIterator) Next() *RedeemQuote {
+	if i.HasNext() {
+		idx := i.curr
+		i.curr += 1
+
+		return i.values[idx]
+	}
+
+	return nil
+}
+
+// Reset resets the iterator
+func (i *RedeemQuoteIterator) Reset() {
+	i.curr = 0
+}
+
+// Add adds an item to the iterator
+func (i *RedeemQuoteIterator) Add(item *RedeemQuote) {
+	i.values = append(i.values, item)
+}
+
 // RedeemQuote represents a redeem quote
 type RedeemQuote struct {
 	wrapped *types.RedeemQuote
